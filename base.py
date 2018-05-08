ddd3357e8136a17f9edd870e49691bca7a6f5307
superspeeder101 <36306837+superspeeder101@users.noreply.github.com>
2018-05-08 16:47:45 -0700

    Update base.py

--- base.py
+++ base.py
@@ -1,2 +1,17 @@
+class BaseParser(object):
+  pass
+
+class DataContainer(object):
+  pass
+
+class Conditional(object):
+  pass
+
+class EnumObject(object):
+  pass
+
 class Enum(object):
+  pass
+
+class Decider(object):
   pass
