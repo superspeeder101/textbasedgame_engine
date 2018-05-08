216cd255d801fab982023be5ffb32ba99e8dda21
superspeeder101 <36306837+superspeeder101@users.noreply.github.com>
2018-05-08 16:44:30 -0700

    Update engine.py

--- engine.py
+++ engine.py
@@ -1,2 +1,37 @@
+import base
 
-class 
+class Location(object):
+  pass
+
+class Event(object):
+  pass
+
+class Action(object):
+  pass
+
+class Item(object):
+  pass
+
+class Inventory(object):
+  pass
+
+class MainParser(base.BaseParser):
+  pass
+
+class Map(object):
+  pass
+
+class Game(base.DataContainer):
+  pass
+
+class Result(base.Conditional):
+  pass
+
+class State(base.EnumObject):
+  pass
+
+class StateEnum(base.Enum):
+  pass
+
+class StateMachine(base.Decider):
+  pass
