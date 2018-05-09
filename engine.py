da95b2268912d6127cbc744077ad380a531b6b85
superspeeder101 <36306837+superspeeder101@users.noreply.github.com>
2018-05-09 12:01:11 -0700

    Update engine.py

--- engine.py
+++ engine.py
@@ -1,8 +1,14 @@
 import base
 
 class Location(object):
-  pass
-
+  def __init__(self, x, y, name, game):
+    self.game = game
+    self.name = name
+    self.pos = (x,y)
+    
+    
+    self.game.registerLocation(self, self.name, self.pos)
+    
 class Event(object):
   pass
 
@@ -12,7 +18,7 @@ class Action(object):
 class Item(object):
   pass
 
-class Inventory(object):
+class Inventory(base.DataContainer):
   pass
 
 class MainParser(base.BaseParser):
@@ -22,9 +28,17 @@ class Map(object):
   pass
 
 class Game(base.DataContainer):
-  pass
+  def __init__(self, map_, stateMachine, inventory):
+    self.map = map
+    self.stateMachine = stateMachine
+    self.inventory = inventory
+    
+    self.registerType("location", cls=Location, adder=self._locationAdder, remover=self._locationRemover)
+    self.registerType("item", cls=Item, adder=self.inventory.addItem, remover=self.inventory.removeItem)
+    self.registerType("action", cls=Action, adder=self._actionAdder, remover=self._actionRemover)
+    
 
-class Result(base.Conditional):
+class StateCondition(base.Conditional):
   pass
 
 class State(base.EnumObject):
@@ -34,4 +48,10 @@ class StateEnum(base.Enum):
   pass
 
 class StateMachine(base.Decider):
+  def __init__(self, stateEnum):
+    self.optionEnum = stateEnum
+    
+    self.generateConditionTester()
+
+class Scene(base.DataContainer):
   pass
